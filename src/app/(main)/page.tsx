bc45a997489f81f740835d6a2ea3b74a7e972221
Soyeon <isylsy166@gmail.com>
2025-01-27 15:54:37 +0900

    임시파일 삭제

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,10 @@
 import styles from "./page.module.css";
 import layoutstyle from "@/styles/layout.module.css"
-import PostList from "@/components/PostList";
 
 export default function Home() {
   return (
     <div className={layoutstyle.page_frame}>
-      <div>홈 화면 엥</div>
-      <PostList/>
+      <div>홈 화면</div>
     </div>
   );
 }
